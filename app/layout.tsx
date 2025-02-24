da955287fabf03f6f201508dbedbe27de2ec3525
dwang48 <michaelwang970403@gmail.com>
2025-02-23 22:06:12 -0500

    update logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <SessionProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
--- components/sections/hero-landing.tsx
+++ components/sections/hero-landing.tsx
@@ -27,7 +27,7 @@ export default async function HeroLanding() {
     <section className="space-y-6 py-12 sm:py-20 lg:py-20">
       <div className="container flex max-w-5xl flex-col items-center gap-5 text-center">
         {/* Want animations? Check here: https://github.com/mickasmt/next-saas-stripe-starter/blob/76eb9f2b70b29c7a734ff0e5b047796ed2dac28d/app/(marketing)/page.tsx */}
-        <Link
+        {/* <Link
           href="https://x.com/Blitzbuild_soft"
           className={cn(
             buttonVariants({ variant: "outline", size: "sm", rounded: "full" }),
@@ -37,7 +37,7 @@ export default async function HeroLanding() {
         >
           <span className="mr-3">🎉</span> Introducing on{" "}
           <Icons.twitter className="ml-2 size-3.5" />
-        </Link>
+        </Link> */}
 
         <h1 className="text-balance font-urban text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl lg:text-[66px]">
           Innovate at the Speed of{" "}
